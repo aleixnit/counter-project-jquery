0495fc397f94af1450bb3f60fb8ee42aac238faa
aleixnit <nitjac210@gmail.com>
2023-05-09 11:12:49 +0200

    ejercicio counter con JQUERY hecho

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 let counter = 0;
 
 $(document).ready(function () {
-    console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
+  console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
+  $(".prevBtn").click(function () {
+    counter--;
+    $("#counter").text(counter);
+    coloresCounter();
+  });
+
+  $(".nextBtn").click(function () {
+    counter++;
+    $("#counter").text(counter);
+    coloresCounter();
+  });
+
+  function coloresCounter() {
+    if (counter === 0) {
+      $("#counter").removeClass("positivo");
+      $("#counter").removeClass("negativo");
+    } else if (counter > 0) {
+      $("#counter").addClass("positivo");
+    } else {
+      $("#counter").addClass("negativo");
+    }
+  }
 });
